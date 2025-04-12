4e340f4a99a72a82ec9ce4f6f1d12a82ec7a1e75
Yogi@gag <dr.yogeesh31@gmail.com>
2025-04-12 14:40:19 +0530

    [YOGEESH] Updated : Used Java Object Oriented

---
+++ Point.java
@@ -0,0 +1,14 @@
+package com;
+
+public class Point {
+	  // attributes- coordinates
+    int x;
+    int y;
+
+    // constructor for initializing the attributes
+     Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+}
